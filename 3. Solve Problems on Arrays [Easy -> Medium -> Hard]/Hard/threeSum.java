528e94d76bf1ac943d7485db5afe0b5fba1ade3e
Shivam Rakesh Vishwakarma <shivamvishwakarma1432@gmail.com>
2023-04-27 08:50:36 +0530

    Updated

--- 3. Solve Problems on Arrays [Easy -> Medium -> Hard]/Hard/threeSum.java
+++ 3. Solve Problems on Arrays [Easy -> Medium -> Hard]/Hard/threeSum.java
@@ -13,8 +13,7 @@ public class threeSum {
                     }
                 }
             }
-        }
-        
+        }       
     }
     public static void main(String[] args) {
         int arr[] = {-1,0,1,0};
