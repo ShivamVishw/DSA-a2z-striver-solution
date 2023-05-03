54e77cffe02c42de10620d163e246eb490ed1076
Shivam Rakesh Vishwakarma <shivamvishwakarma1432@gmail.com>
2023-05-03 15:53:02 +0530

    Updated

--- Binary Search [1D, 2D Arrays, Search Space]/Learning BS on 1D Arrays/searchInRotatedSorted.java
+++ Binary Search [1D, 2D Arrays, Search Space]/Learning BS on 1D Arrays/searchInRotatedSorted.java
@@ -26,8 +26,8 @@ public class searchInRotatedSorted {
         return -1;
     }
     public static void main(String[] args) {
-        int arr[]= {4,5,6,7,1,2,3};
-        int target = 2;
+        int arr[]= {4,5,6,7,0,1,2,3};
+        int target = 5;
         System.out.println(searchSRotatedSorted(arr, target));        
     }
     // This program is exception for 0 integer 
