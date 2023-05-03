a5344498769dd6cba48e7ce734833a135eabcdba
Shivam Rakesh Vishwakarma <shivamvishwakarma1432@gmail.com>
2023-05-03 15:54:31 +0530

    Updated

--- Binary Search [1D, 2D Arrays, Search Space]/Learning BS on 1D Arrays/minElementInRotated.java
+++ Binary Search [1D, 2D Arrays, Search Space]/Learning BS on 1D Arrays/minElementInRotated.java
@@ -30,7 +30,7 @@ public class minElementInRotated {
     }
 
     public static void main(String[] args) {
-        int arr[]={2,3,4};
+        int arr[]={4,5,6,7,1,2,3};
         System.out.println("Minimum element in the rotated sorted array is "+minRotated(arr));
     }
 }
