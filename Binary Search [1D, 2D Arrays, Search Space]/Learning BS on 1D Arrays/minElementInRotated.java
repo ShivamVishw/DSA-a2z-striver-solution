93f80de460ffaa74087d729b67c410f3b9c5dfbb
Shivam Rakesh Vishwakarma <shivamvishwakarma1432@gmail.com>
2023-05-03 15:52:32 +0530

    Minimum  element in rotated sorted array

---
+++ Binary Search [1D, 2D Arrays, Search Space]/Learning BS on 1D Arrays/minElementInRotated.java
@@ -0,0 +1,36 @@
+public class minElementInRotated {
+    static int minRotated(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        // int first = 0;
+        // int last = arr[arr.length-1];
+        int ans =0;
+    
+        while(start<=end){
+            if(arr.length==2){
+                ans = Math.min(start, end);
+                return ans;
+            }else{
+                //find the middle element
+                // int mid = (start + end) / 2; // because might be possible that, 
+                // (start + end) exceeds the range of int in java
+                int mid = start +(end-start)/2;
+            
+                if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                return arr[mid+1];
+
+                }else if(arr[mid]>arr[mid-1]){
+                    start = mid +1;
+                }else{
+                    end= mid -1;
+                }           
+            }
+            
+        }return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={2,3,4};
+        System.out.println("Minimum element in the rotated sorted array is "+minRotated(arr));
+    }
+}
