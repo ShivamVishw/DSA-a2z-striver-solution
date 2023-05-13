1a6177c4e3b8f1eae1259af6815d5a2f4f4b2618
Shivam Rakesh Vishwakarma <shivamvishwakarma1432@gmail.com>
2023-05-13 13:23:15 +0530

    Updated

---
+++ Learn LinkedList [Single/Learn 1D LinkedList/insertFirst.java
@@ -0,0 +1,41 @@
+class insertAtFirst {
+    class Node {
+        int data;
+        Node next;
+    }
+    Node head;
+
+    // Function to add element at the first position in the linkedList
+    void addFirst(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+
+        //If head is equal to null then newNode will automatically be first element
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    void printList() {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        insertAtFirst list = new insertAtFirst();
+
+        list.addFirst(23);
+        list.addFirst(24);
+        list.addFirst(25);
+        list.addFirst(28);
+
+        list.printList();
+    }
+}
